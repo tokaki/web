a26b5bd14b7006bbe0bd4fa364265c7ecc6c0cc6
tokaki <thaweesap2012@gmail.com>
2017-02-01 10:17:06 +0000

    first commit

---
+++ web.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "page1.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "page2.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "page3.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
